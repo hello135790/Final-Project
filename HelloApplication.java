df4134f583dba7253ef8dba1de0e9042502807c3
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-23 16:08:06 +0000

    Working on layout

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,34 +1,38 @@
 package com.example.demo;
 
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
-    Button button;
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        primaryStage.setTitle("Title");
-        button = new Button();
-        button.setText("Click me");
+   Button button;
+   @Override
+   public void start(Stage primaryStage) throws Exception {
+       primaryStage.setTitle("Title");
+       button = new Button();
+       button.setText("Click me");
+       Pane root = new Pane();
+       button.setLayoutX(50);
+       button.setLayoutY(50);
+//        StackPane layout = new StackPane();
+       root.getChildren().add(button);
+       Scene scene = new Scene(layout, 300, 250);
+       primaryStage.setScene(scene);
+       primaryStage.show();
+   }
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
 
-
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+   public static void main(String[] args) {
+       launch();
+   }
 }
